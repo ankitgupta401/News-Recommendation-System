528796ce5fc6e123e9db7218fbb8925eadc8137f
Ankit Gupta <ag3831124@gmail.com>
2023-02-12 13:49:55 +0530

    fixes

--- crons/fetchNews.cron.js
+++ crons/fetchNews.cron.js
@@ -2,5 +2,6 @@ const cron = require("node-cron");
 
 cron.schedule("0 */4 * * *", async () => {
   let port = process.env.PORT || 4000;
-  await axios.get(`http://localhost:${port}/v1/api/news/add-news-from-api`);
+  console.log("running Cron")
+  await axios.get(`https://http-nodejs-production-dfb7.up.railway.app/v1/api/news/add-news-from-api`);
 });
