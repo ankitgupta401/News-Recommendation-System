d67d853d882ee1ae647bce4cef648a9c50944614
Ankit Gupta <ag3831124@gmail.com>
2023-02-21 21:00:44 +0530

    cron update

--- crons/fetchNews.cron.js
+++ crons/fetchNews.cron.js
@@ -1,7 +1,9 @@
 const cron = require("node-cron");
 
-cron.schedule("0 */4 * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
   let port = process.env.PORT || 4000;
   console.log("running Cron")
-  await axios.get(`https://http-nodejs-production-dfb7.up.railway.app/v1/api/news/add-news-from-api`);
+  axios.get(`https://http-nodejs-production-dfb7.up.railway.app/v1/api/news/add-news-from-api`).then((res) => {
+    console.log(res.data);
+  });
 });
