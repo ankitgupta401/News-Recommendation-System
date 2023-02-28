beba306f8c459f399cc1714f2cfbb4aa33a09f35
Ankit Gupta <ag3831124@gmail.com>
2023-02-28 19:36:03 +0530

    fixes

--- models/News.model.js
+++ models/News.model.js
@@ -9,7 +9,7 @@ const newsSchema = mongoose.Schema({
   views: { type: Number, default: 0 },
   engagements: { type: Number, default: 0 },
   averageViewDuration: { type: Number, default: 0 },
-  interestName: { type: String, required: false, unique: true },
+  interestName: { type: String, required: true },
   isDeleted: { type: Boolean, default: false },
 }, { timestamps: true });
 
